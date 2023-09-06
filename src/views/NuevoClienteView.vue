31e003ff6fcbce97a2684069720be5288be12fff
andres.mera <feli041089@gmail.com>
2023-09-06 07:58:32 -0500

    Agregando campos al formulario y mas reglas de val

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -25,10 +25,18 @@ defineProps({
             <div class="mx-auto md:w-2/3 py-20 px-6">
                 <FormKit type="form">
 
-                    <FormKit type="text" label="Nombre" placeholder="Nombre del cliente"
-                        help="Coloca el nombre del usuario que deseas registrar" validation="required"
+                    <FormKit type="text" label="Nombre" placeholder="Nombre del cliente" validation="required"
                         :validation-messages="{ required: 'El Nombre del Cliente es Obligatorio.' }" />
-                        
+
+                    <FormKit type="text" label="Apellido" placeholder="Apellido del cliente" validation="required"
+                        :validation-messages="{ required: 'El Apellido del Cliente es Obligatorio.' }" />
+
+                    <FormKit type="email" label="Email" placeholder="Email del cliente" validation="required|email"
+                        :validation-messages="{ required: 'El Email del Cliente es Obligatorio.', email: 'Coloca un Email válido' }" />
+
+                    <FormKit type="text" label="Teléfono" placeholder="Teléfono: XXX-XXX-XXXX" validation="required|email"
+                        :validation-messages="{ required: 'El Teléfono del Cliente es Obligatorio.', email: 'Coloca un Email válido' }" />
+
                 </FormKit>
             </div>
         </div>
