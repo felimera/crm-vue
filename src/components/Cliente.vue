68f84579f4e53c6e80ca2e953c0788a4642f51c5
andres.mera <feli041089@gmail.com>
2023-09-06 09:55:09 -0500

    Mostrar informacion del cliente part 01

---
+++ src/components/Cliente.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+    cliente: {
+        type: Object,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <tr>
+        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-0">
+            <p class="font-medium text-gray-900">{{ cliente.nombre }} {{ cliente.apellido }}</p>
+            <p class="text-gray-500"></p>
+        </td>
+        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+            <p class="text-gray-900 font-bold"></p>
+            <p class="text-gray-600"></p>
+        </td>
+        <td class="whitespace-nowrap px-3 py-4 text-sm">
+
+        </td>
+        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 ">
+
+        </td>
+    </tr>
+</template>
