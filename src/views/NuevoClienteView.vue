55e71339adbc298ccb69d4dd078f68c6b188293a
andres.mera <feli041089@gmail.com>
2023-09-06 08:37:08 -0500

    Manejando el Submit y leer datos del formulario

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -10,6 +10,10 @@ defineProps({
     }
 });
 
+const handleSubmit = data => {
+
+}
+
 </script>
 
 <template>
@@ -24,24 +28,26 @@ defineProps({
         <div class="mx-auto mt-10 bg-white shadow">
             <div class="mx-auto md:w-2/3 py-20 px-6">
                 <FormKit type="form" submit-label="Agregar Cliente"
-                    incomplete-message="No se pudo enviar, revisa los mensajes">
+                    incomplete-message="No se pudo enviar, revisa los mensajes" @submit="handleSubmit">
 
-                    <FormKit type="text" label="Nombre" placeholder="Nombre del cliente" validation="required"
+                    <FormKit type="text" label="Nombre" name="nombre" placeholder="Nombre del cliente" validation="required"
                         :validation-messages="{ required: 'El Nombre del Cliente es Obligatorio.' }" />
 
-                    <FormKit type="text" label="Apellido" placeholder="Apellido del cliente" validation="required"
+                    <FormKit type="text" label="Apellido" name="apellido" placeholder="Apellido del cliente"
+                        validation="required"
                         :validation-messages="{ required: 'El Apellido del Cliente es Obligatorio.' }" />
 
-                    <FormKit type="email" label="Email" placeholder="Email del cliente" validation="required|email"
+                    <FormKit type="email" label="Email" name="email" placeholder="Email del cliente"
+                        validation="required|email"
                         :validation-messages="{ required: 'El Email del Cliente es Obligatorio.', email: 'Coloca un Email válido' }" />
 
-                    <FormKit type="text" label="Teléfono" placeholder="Teléfono: XXX-XXX-XXXX"
+                    <FormKit type="text" label="Teléfono" name="telefono" placeholder="Teléfono: XXX-XXX-XXXX"
                         validation="*matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
                         :validation-messages="{ matches: 'El formato no es válido' }" />
 
-                    <FormKit type="text" label="Empresa" placeholder="Empresa del cliente" />
+                    <FormKit type="text" label="Empresa" name="empresa" placeholder="Empresa del cliente" />
 
-                    <FormKit type="text" label="Puesto" placeholder="Puesto del cliente" />
+                    <FormKit type="text" label="Puesto" name="puesto" placeholder="Puesto del cliente" />
 
                 </FormKit>
             </div>
